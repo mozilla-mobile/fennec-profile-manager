4bfe26ecd080ec04f354cbd2e0a4721b38313857
Mugurell <mugurell@users.noreply.github.com>
2019-10-31 19:04:00 +0200

    For #4 - Basic POC for displaying Fennec's internal storage files

--- app/src/main/java/org/mozilla/fennecprofilebackuphelper/MainActivity.kt
+++ app/src/main/java/org/mozilla/fennecprofilebackuphelper/MainActivity.kt
@@ -4,10 +4,12 @@
 
 package org.mozilla.fennecprofilebackuphelper
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +18,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val fennecContext = createPackageContext("org.mozilla.firefox", Context.CONTEXT_RESTRICTED)
+        val fennecInternalStorage = File(fennecContext.filesDir.parent)
+
+        fab.setOnClickListener {
+            description.text = getString(R.string.poc_results_description)
+            description.textSize = 16f
+            results.text = fennecInternalStorage.list().joinToString("\n")
+            fab.hide()
         }
     }
 }
