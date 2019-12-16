abbfc56b11bd329f51f94543bcbb2d29541ea4bf
Vlad Baicu <vladmariobaicu@gmail.com>
2019-12-16 18:15:27 +0200

    ui improvements

--- app/src/main/java/org/mozilla/fpm/presentation/BackupsRVAdapter.kt
+++ app/src/main/java/org/mozilla/fpm/presentation/BackupsRVAdapter.kt
@@ -4,6 +4,7 @@
 
 package org.mozilla.fpm.presentation
 
+import android.text.format.Formatter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -68,7 +69,7 @@ class BackupsRVAdapter : RecyclerView.Adapter<BackupsRVAdapter.BackupViewHolder>
             val backup: Backup = dataSource[position]
             title.text = backup.name.replace(".zip", "")
             date.text = backup.createdAt
-            size.text = String.format(itemView.resources.getString(R.string.file_size), backup.size)
+            size.text = Formatter.formatShortFileSize(itemView.context, backup.size)
             apply.setOnClickListener { listener.onApplyClick(backup) }
             share.setOnClickListener { listener.onShareClick(backup) }
             edit.setOnClickListener { listener.onEditClick(backup, position) }
